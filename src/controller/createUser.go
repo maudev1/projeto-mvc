383dea747a87e8fa5ca0d3deb0b69dfeebd498cf
mauriciojr <mauriciojr.dev@gmail.com>
2025-05-21 12:37:08 -0300

    Create User Controller

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/maudev1/meu-primeiro-crud/src/configuration/rest_err"
+	"github.com/maudev1/meu-primeiro-crud/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	// var userRequest request.UserRequest
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindBodyWithJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect field, erro=%s\n", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+
+		return
+	}
+
+	fmt.Println(userRequest)
 
 	//aula 6
 
